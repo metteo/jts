d4b037903cd9ff1a64a124ceaa01ad37e56425f1
metteo <grz3gorz.nowak@gmail.com>
2014-08-23 17:31:38 +0200

    Initial commit of jts-io-oracle project.

---
+++ jts-io-oracle/src/test/java/com/vividsolutions/jts/io/oracle/BaseOraTestCase.java
@@ -0,0 +1,53 @@
+/*
+ * The JTS Topology Suite is a collection of Java classes that
+ * implement the fundamental operations required to validate a given
+ * geo-spatial data set to a known topological specification.
+ *
+ * Copyright (C) 2001 Vivid Solutions
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ *
+ * For more information, contact:
+ *
+ *     Vivid Solutions
+ *     Suite #1A
+ *     2328 Government Street
+ *     Victoria BC  V8T 5G5
+ *     Canada
+ *
+ *     (250)385-6040
+ *     www.vividsolutions.com
+ */
+package com.vividsolutions.jts.io.oracle;
+
+import junit.framework.TestCase;
+
+/**
+ * Tests OraReader without requiring an Oracle connection.
+ * 
+ * @author mbdavis
+ *
+ */
+public class BaseOraTestCase extends TestCase
+{
+  public BaseOraTestCase(String name)
+  {
+    super(name);
+  }
+
+  protected static final int NULL = -1;
+  protected static final double DNULL = Double.NaN;
+
+}
---
+++ jts-io-oracle/src/test/java/com/vividsolutions/jts/io/oracle/MDSYS.java
@@ -0,0 +1,38 @@
+package com.vividsolutions.jts.io.oracle;
+
+/**
+ * Mimics Oracle MDSYS functions for building geometries.
+ * Useful for creating test objects.
+ * 
+ * @author mbdavis
+ *
+ */
+public class MDSYS {
+
+  protected static final int NULL = -1;
+
+	public static OraGeom SDO_GEOMETRY(int gType, int srid, int ptType,
+			int[] elemInfo, double[] ordinates) {
+		return new OraGeom(gType, srid, elemInfo, ordinates);
+	}
+
+	public static OraGeom SDO_GEOMETRY(int gType, int srid, double[] ptType,
+			int null1, int null2) {
+		return new OraGeom(gType, srid, ptType);
+	}
+
+	public static double[] SDO_POINT_TYPE(double x, double y, double z) {
+	  if (z == NULL) z = Double.NaN;
+		return new double[] { x, y, z };
+	}
+
+	public static int[] SDO_ELEM_INFO_ARRAY(int... i) {
+		return i;
+	}
+	
+	public static double[] SDO_ORDINATE_ARRAY(double... d) {
+		return d;
+	}
+
+
+}
