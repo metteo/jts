e3be98fef0a4fa2f3693034b0582ff9eee23d096
metteo <grz3gorz.nowak@gmail.com>
2015-03-30 18:14:40 +0200

    Fixes after changing GeoJsonReader interface.

--- jts-webapp/src/main/java/com/vividsolutions/jtswebapp/client/GeoJson.java
+++ jts-webapp/src/main/java/com/vividsolutions/jtswebapp/client/GeoJson.java
@@ -1,8 +1,10 @@
 package com.vividsolutions.jtswebapp.client;
 
+import java.util.logging.Level;
 import java.util.logging.Logger;
 
 import com.vividsolutions.jts.geom.Geometry;
+import com.vividsolutions.jts.io.ParseException;
 import com.vividsolutions.jts.io.geojson.GeoJsonReader;
 
 public class GeoJson {
@@ -11,7 +13,13 @@ public class GeoJson {
 
 	public void check() {
 		GeoJsonReader r = new GeoJsonReader();
-		Geometry g = r.read("{\"type\":\"Point\", \"coordinates\":[10, 20]}");
-		sLogger.info(g.toString());
+		Geometry g;
+		try {
+			g = r.read("{\"type\":\"Point\", \"coordinates\":[10, 20]}");
+			sLogger.info(g.toString());
+		} catch (ParseException e) {
+			sLogger.log(Level.WARNING, "Problem while parsing GeoJSON", e);
+		}
+		
 	}
 }
